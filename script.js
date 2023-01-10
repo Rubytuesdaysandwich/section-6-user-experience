93da4bddc2d2bc8073adf433e385509f148b449d
Rubytuesdaysandwich <rueben.durfee@gmail.com>
2023-01-10 16:30:51 -0700

    update almost complete

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 const slider= document.querySelector(".slider input")
-const dragLine= document.querySelector(".slider drag-line")
+const img = document.querySelector(".images .img-2")
+const dragLine= document.querySelector(".slider .drag-line")
 slider.oninput = ()=>{
     let sliderVal = slider.value;
-    dragLine.getElementsByClassName.left=sliderVal + "%"
+    dragLine.style.left=sliderVal + "%";
+    img.style.width=sliderVal + "%";
 }
