6936182828f98105ee6fdc6080efc3890f8416fd
Rubytuesdaysandwich <rueben.durfee@gmail.com>
2023-01-10 15:11:30 -0700

    update

--- image-comparison.js
+++
@@ -1,9 +0,0 @@
-function comparisons(){
-
-    const overlay = document.querySelectorAll(".overlay")
-    for (i = 0; i <overlay.length;i++){
-compareImages(overlay[i]);
-    }
-}
-
-console.log("end of script reached")
